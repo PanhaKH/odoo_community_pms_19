23901fddb5119b031745a6cbbcdd397cead8771e
channachhom007-hue <channa.chhom007@gmail.com>
2026-06-06 14:45:47 +0700

    check version

--- hotel_management/__manifest__.py
+++ hotel_management/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Hotel Management System',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'summary': 'Complete Hotel Reservation, Front Desk, and Housekeeping System',
     'sequence': -100,
     'description': """Hotel Management Solution""",
